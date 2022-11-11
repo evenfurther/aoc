1cf0a362ab5ea23cd55bdcaf085c92cefaa92226
Samuel Tardieu <sam@rfc1149.net>
2022-11-11 19:05:47 +0100

    Import tests

---
+++ aoc/tests/aoc.rs
@@ -0,0 +1,140 @@
+use serial_test::serial;
+use std::str::FromStr;
+
+use aoc_derive::aoc;
+
+#[aoc(day1, part1)]
+fn day1_part1() -> usize {
+    30
+}
+
+#[aoc(day1, part2)]
+fn day1_part2() -> eyre::Result<usize> {
+    Ok(40)
+}
+
+#[aoc(day1, part2, alternate)]
+fn day1_part2_alternate() -> eyre::Result<usize> {
+    Ok(50)
+}
+
+#[test]
+fn result() {
+    assert_eq!(30, day1_part1());
+    assert_eq!(40, day1_part2().unwrap());
+    assert_eq!(50, day1_part2_alternate().unwrap());
+}
+
+#[test]
+fn runner() {
+    assert_eq!(30, runner_1_1_none().unwrap());
+    assert_eq!(40, runner_1_2_none().unwrap());
+    assert_eq!(50, runner_1_2_alternate().unwrap());
+}
+
+#[aoc(day3, part1, str)]
+fn d3p1s(input: &str) -> String {
+    input.replace('\n', "")
+}
+
+#[aoc(day3, part1, vec_str)]
+fn d3p1svec(mut input: Vec<&str>) -> String {
+    input.sort_unstable();
+    format!("{:?}", input)
+}
+
+#[aoc(day3, part1, ref_str)]
+fn d3p1sref(input: &[&str]) -> String {
+    format!("{:?}", input)
+}
+
+#[aoc(day3, part1, mut_str)]
+fn d3p1smut(input: &mut [&str]) -> String {
+    input.sort_unstable();
+    format!("{:?}", input)
+}
+
+#[aoc(day3, part1, bytes)]
+fn d3p1b(input: &[u8]) -> eyre::Result<String> {
+    Ok(d3p1s(std::str::from_utf8(input)?))
+}
+
+#[aoc(day3, part1, u32)]
+fn d3p1u32(input: &[u32]) -> u32 {
+    input.iter().sum()
+}
+
+#[aoc(day3, part1, u32_commas, separator = ',')]
+fn d3p1u32_commas(input: &[u32]) -> u32 {
+    input.iter().sum()
+}
+
+#[aoc(day3, part1, s)]
+fn d1p1ss(input: &[S]) -> u8 {
+    input.iter().map(|s| s.u).sum()
+}
+
+#[aoc(day3, part1, s_vec)]
+fn d1p1ss_vec(input: Vec<S>) -> u8 {
+    input.into_iter().map(|s| s.u).sum()
+}
+
+#[aoc(day3, part1, s_mut)]
+fn d1p1ss_mut(input: &mut [S]) -> u8 {
+    input.iter().map(|s| s.u).sum()
+}
+
+#[aoc(day3, part1, separator = ',', s_commas)]
+fn d1p1ss_commas(input: &[S]) -> u8 {
+    input.iter().map(|s| s.u).sum()
+}
+
+struct S {
+    u: u8,
+}
+
+impl FromStr for S {
+    type Err = std::io::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ok(S {
+            u: s.as_bytes()[0] - b'0',
+        })
+    }
+}
+
+#[test]
+#[serial]
+fn inputs() {
+    unsafe {
+        aoc::input::OVERRIDE_INPUT = Some(String::from("tests/input.txt"));
+    }
+    assert_eq!("123102030", runner_3_1_str().unwrap());
+    assert_eq!(
+        r#"["1", "10", "2", "20", "3", "30"]"#,
+        runner_3_1_vec_str().unwrap()
+    );
+    assert_eq!(
+        r#"["1", "2", "3", "10", "20", "30"]"#,
+        runner_3_1_ref_str().unwrap()
+    );
+    assert_eq!(
+        r#"["1", "10", "2", "20", "3", "30"]"#,
+        runner_3_1_mut_str().unwrap()
+    );
+    assert_eq!("123102030", runner_3_1_bytes().unwrap());
+    assert_eq!(66, runner_3_1_u32().unwrap());
+    assert_eq!(12, runner_3_1_s().unwrap());
+    assert_eq!(12, runner_3_1_s_vec().unwrap());
+    assert_eq!(12, runner_3_1_s_mut().unwrap());
+}
+
+#[test]
+#[serial]
+fn inputs_commas() {
+    unsafe {
+        aoc::input::OVERRIDE_INPUT = Some(String::from("tests/input-commas.txt"));
+    }
+    assert_eq!(66, runner_3_1_u32_commas().unwrap());
+    assert_eq!(12, runner_3_1_s_commas().unwrap());
+}
