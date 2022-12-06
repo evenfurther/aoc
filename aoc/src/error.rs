51e94b5322156a3c006a0d5af2cf432af1c04bc0
Samuel Tardieu <sam@rfc1149.net>
2022-12-06 11:53:46 +0100

    Better error messages

--- aoc/src/error.rs
+++ aoc/src/error.rs
@@ -2,11 +2,11 @@ use std::string::FromUtf8Error;
 
 #[derive(Debug, thiserror::Error)]
 pub enum Error {
-    #[error("IO error")]
+    #[error("IO error while loading input")]
     Io(#[from] std::io::Error),
-    #[error("malformed UTF8 string")]
+    #[error("malformed UTF8 string in input")]
     Utf8(#[from] FromUtf8Error),
-    #[error("fatal error")]
+    #[error(transparent)]
     Other(#[from] Box<dyn std::error::Error + Send + Sync + 'static>),
     #[error("no output for day {0} part {1}{}", .2.map(|v| format!(r#" (variant "{v}""#)).unwrap_or_default())]
     NoOutput(usize, usize, Option<&'static str>),
