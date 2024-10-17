661c849ffff367eb5413f608ef6e13435605b717
Samuel Tardieu <sam@rfc1149.net>
2024-10-17 15:25:30 +0200

    fix(deps): use proc-macro-error2 instead of the unmaintained proc-macro-error

--- aoc-derive/src/lib.rs
+++ aoc-derive/src/lib.rs
@@ -1,7 +1,7 @@
 #![deny(clippy::pedantic)]
 
 use proc_macro::TokenStream;
-use proc_macro_error::{abort, proc_macro_error};
+use proc_macro_error2::{abort, proc_macro_error};
 use quote::{quote, ToTokens};
 use syn::{
     parse::{Parse, ParseStream},
